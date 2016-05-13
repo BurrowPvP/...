ee2d842d578ccd28563a566e3b24c3f8d76b4092
PaulchenHD <paulchen_hd@web.de>
2016-05-13 03:37:36 -1100

    Update Main.php

--- Main.php
+++ Main.php
@@ -15,6 +15,7 @@ Class Main extends Pluginbase implements Listener{
     public function onLoad(){
         $this->getLogger()->info(TextFormat::YELLOW."Kits-Saving");
         @mkdir($this->getDataFolder() . "Kits/");
+        $this->saveResource("Kits/");
             }
     public function onEnable(){
         $this->getServer()->getPluginManager()->registerEvents($this, $this); 
