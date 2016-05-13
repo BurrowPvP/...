7af77634618d2c71da8f63750f38748f239598fd
PaulchenHD <paulchen_hd@web.de>
2016-05-13 03:03:02 -1100

    Update Main.php

--- Main.php
+++ Main.php
@@ -6,7 +6,10 @@ namespace onevsone;
 
 Class Main extends Pluginbase implements Listener{
     
-    
+    public function onLoad(){
+        $this->getLogger()->info(TextFormat::YELLOW."Kits-Saving");
+        @mkdir($this->getDataFolder() . "Kits/");
+            }
     public function onEnable(){
         $this->getServer()->getPluginManager()->registerEvents($this, $this); 
     }
