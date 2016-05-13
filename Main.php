9669f85224a95e5a58defef7bd7defe887961d48
PaulchenHD <paulchen_hd@web.de>
2016-05-13 03:20:56 -1100

    Update Main.php

--- Main.php
+++ Main.php
@@ -20,14 +20,37 @@ Class Main extends Pluginbase implements Listener{
         $this->getServer()->getPluginManager()->registerEvents($this, $this); 
     }
     public function onDisable(){
-        
+        // onDisable
     }
     //wenn der Spieler auf das schild klickt..
     public function onPlayerInteract(PlayerInteractEvent $event){
 	$b = $event->getBlock();
         $player = $event->getPlayer();
         if($b->getID() == 63 || $b->getID() == 68){
-        	// warteschlange hinzufügen
                         }
     }
+    // chat format von dem lobby-core
+    public function onChat(PlayerChatEvent $event){
+    
+    $player = $event->getPlayer();
+    $nachricht = $event->getMessage();
+    
+        if($player->hasPermission("Chat.Spieler")) {
+            $event->setFormat( "Â§e" . $player->getName() . " Â§8Â»Â§7 " .$nachricht);      
+        }
+
+        if($player->hasPermission("Chat.Premium")) {
+            $event->setFormat( "Â§6" . $player->getName() . " Â§8Â»Â§7 " .$nachricht);       
+        }
+        if($player->hasPermission("Chat.Supporter")) {
+            $event->setFormat( "Â§9" . $player->getName() . " Â§8Â»Â§7 " .$nachricht);       
+        }
+        if($player->hasPermission("Chat.Moderator")) {
+            $event->setFormat( "Â§c" . $player->getName() . " Â§8Â»Â§7 " .$nachricht);       
+        }
+        if($player->hasPermission("Chat.Admin")) {
+            $event->setFormat( "Â§4" . $player->getName() . " Â§8Â»Â§7 " .$nachricht);       
+        }
+
+}
 }
