9c28b341d611ca0bc67113a97aaeb485e038a851
PaulchenHD <paulchen_hd@web.de>
2016-05-13 03:15:07 -1100

    Update Main.php

--- Main.php
+++ Main.php
@@ -1,9 +1,15 @@
 <?php
 
-
+/*
+ ____ _____  _    ____ _____ 
+/ ___|_   _|/ \  |  _ \_   _|
+\___ \ | | / _ \ | |_) || |  
+ ___) || |/ ___ \|  _ < | |  
+|____/ |_/_/   \_\_| \_\|_| 
+ */ 
+                             
 namespace onevsone;
-
-
+                             
 Class Main extends Pluginbase implements Listener{
     
     public function onLoad(){
